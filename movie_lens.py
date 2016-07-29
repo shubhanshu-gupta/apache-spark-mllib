fd68e5b162ea8d856c2d14c898acf8edb1bd3a38
Shubhanshu Gupta <shubhanshu2007@gmail.com>
2016-07-29 19:24:56 +0530

    attempt to extract mse out of test.data

--- movie_lens.py
+++ movie_lens.py
@@ -7,6 +7,7 @@ from pyspark.sql import SparkSession
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
 from pyspark.sql import Row
+from numpy import array
 
 spark = SparkSession\
         .builder\
@@ -30,6 +31,21 @@ evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating", prediction
 
 rmse = evaluator.evaluate(predictions)
 print "Root-mean-square error = " + str(rmse)
+spark.stop()
+
+##### Tried working with test.data, but didn't work out. Need to study more ########
+# sc = SparkContext("local", "ALSApp")
+# data = sc.textFile("/home/shubhanshu/Documents/spark-2.0.0-bin-hadoop2.7/data/mllib/als/test.data")
+# test_ratings = data.map(lambda line: array([float(x) for x in line.split(',')]))
+
+# model = ALS(test_ratings, 1, 20)
+
+# testdata = ratings.map(lambda p: (int(p[0]), int(p[1])))
+# test_predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
+# ratesAndPreds = test_ratings.map(lambda r: ((r[0], r[1]), r[2])).join(test_predictions)
+# MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).reduce(lambda x, y: x + y)/ratesAndPreds.count()
+
+# print "Mean Squared Error = " + str(MSE)
 
 #ratings.show()
-# print len(ratings.userId)
+# https://spark.apache.org/docs/0.9.2/mllib-guide.html#collaborative-filtering-2
